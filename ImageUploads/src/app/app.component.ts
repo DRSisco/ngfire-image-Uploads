7d3932d9b42c39b80d52f2d8c5446cecde124e74
Donald Sisco <dododonnars@gmail.com>
2017-04-20 10:30:23 -0400

    Featured Photos

--- ImageUploads/src/app/app.component.ts
+++ ImageUploads/src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { AngularFireModule, FirebaseObjectObservable, AngularFire } from 'angularfire2';
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+interface FeaturedPhotoUrls {
+  url1?: string
+  url2?: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  public featuredPhotosStream: FirebaseObjectObservable<FeaturedPhotoUrls>
+
+
+  constructor(private af: AngularFire) {
+    this.featuredPhotosStream = this.af.database.object("/photos/featured")
+  }
+
+  // featuredPhotoSelected(event: MSInputMethodContext) {
+  //   const target : HTMLInputElement = <HTMLInputElement> event.target
+  //   const files: FileList = target.files
+  //   const file: File  = files[0]
+
+
+  //   console.log("Selected File", file)
+  // }
+
+  featuredPhotoSelected(event: any, photoNumber: string) {
+    const file: File  = event.target.files[0]
+
+    const metadata = {"content-type": file.type}
+    const storageRef: firebase.storage.Reference = firebase.storage().ref().child("photos").child("featured").child(photoNumber)
+    const uploadTask : firebase.storage.UploadTask = storageRef.put(file, metadata)
+    uploadTask.then((snapshot: firebase.storage.UploadTaskSnapshot) => {
+      firebase.database().ref().child("photos").child("featured").child(photoNumber).set(snapshot.downloadURL)
+    })
+
+    console.log("Uploading", file.name)
+  }
+
+  triggerInput(input: HTMLInputElement) {
+    input.click()
+  }
 }
--- ImageUploads/src/app/app.module.ts
+++ ImageUploads/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
+import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
