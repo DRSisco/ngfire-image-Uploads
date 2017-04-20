389b6d28380b3777480cae41b587623b9d908a7c
Donald Sisco <dododonnars@gmail.com>
2017-04-20 09:36:17 -0400

    Initial Commit

---
+++ ImageUploads/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAF5BlN8xu9G70WmmjG7jwmgjNGqqmny6k",
+    authDomain: "siscodr-imageuploads.firebaseapp.com",
+    databaseURL: "https://siscodr-imageuploads.firebaseio.com",
+    projectId: "siscodr-imageuploads",
+    storageBucket: "siscodr-imageuploads.appspot.com",
+    messagingSenderId: "449978905851"
+  }
+};
---
+++ ImageUploads/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAF5BlN8xu9G70WmmjG7jwmgjNGqqmny6k",
+    authDomain: "siscodr-imageuploads.firebaseapp.com",
+    databaseURL: "https://siscodr-imageuploads.firebaseio.com",
+    projectId: "siscodr-imageuploads",
+    storageBucket: "siscodr-imageuploads.appspot.com",
+    messagingSenderId: "449978905851"
+  }
+};
