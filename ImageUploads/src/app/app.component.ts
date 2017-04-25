20a64180e673723ce2710c6e11aa976b5862f79b
Donald Sisco <dododonnars@gmail.com>
2017-04-25 09:04:43 -0400

    Finished

--- ImageUploads/src/app/app.component.ts
+++ ImageUploads/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { AngularFireModule, FirebaseObjectObservable, AngularFire } from 'angularfire2';
+import { AngularFireModule, FirebaseObjectObservable, AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
 
 interface FeaturedPhotoUrls {
   url1?: string
   url2?: string
+}
+
+interface Photo {
+  url: string
 }
 
 @Component({
@@ -14,10 +18,11 @@ interface FeaturedPhotoUrls {
 })
 export class AppComponent {
   public featuredPhotosStream: FirebaseObjectObservable<FeaturedPhotoUrls>
-
+  public photoListStream: FirebaseListObservable<Photo[]>
 
   constructor(private af: AngularFire) {
     this.featuredPhotosStream = this.af.database.object("/photos/featured")
+    this.photoListStream = this.af.database.list("/photos/list")
   }
 
   // featuredPhotoSelected(event: MSInputMethodContext) {
@@ -39,6 +44,19 @@ export class AppComponent {
       firebase.database().ref().child("photos").child("featured").child(photoNumber).set(snapshot.downloadURL)
     })
 
+    console.log("Uploading", file.name)
+  }
+
+  photoSelectedForList(event: any){
+    const nextAvailableKey = firebase.database().ref().child("photos").child("list").push({}).key
+    const file: File  = event.target.files[0]
+    const metadata = {"content-type": file.type}
+    const storageRef: firebase.storage.Reference = firebase.storage().ref().child("photos").child("list").child(nextAvailableKey)
+    const uploadTask : firebase.storage.UploadTask = storageRef.put(file, metadata)
+    uploadTask.then((snapshot: firebase.storage.UploadTaskSnapshot) => {
+      const photo: Photo = {url: snapshot.downloadURL}
+      firebase.database().ref().child("photos").child("list").child(nextAvailableKey).set(photo)
+    })
     console.log("Uploading", file.name)
   }
 
